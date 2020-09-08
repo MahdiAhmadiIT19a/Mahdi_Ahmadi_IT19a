47b86fbd66f108100ed0c942bc6055839f4c2f68
MahdiAhmadiIT19a <70194615+MahdiAhmadiIT19a@users.noreply.github.com>
2020-09-08 09:40:19 +0200

    commit

--- Variabel_IO/index.js
+++ Variabel_IO/index.js
@@ -8,4 +8,8 @@ let tal3 = 5
 
 medelvärde = (tal1+tal2+tal3)/3
 
-console.log(``)
+console.log(`medelvärdet av 3,4,5 är ${medelvärde}`)
+
+console.log(`(${tal1}+${tal2}+${tal3})/3`)
+
+let langd = prompt("ange värdet för rektangelns längd")
