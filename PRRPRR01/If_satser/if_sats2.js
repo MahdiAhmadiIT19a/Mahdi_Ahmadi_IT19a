5501c21fcb6a3bd12973ae6709d2ebcf3ddea733
MahdiAhmadiIT19a <70194615+MahdiAhmadiIT19a@users.noreply.github.com>
2020-09-17 09:53:17 +0200

    den nyaste!

--- PRRPRR01/If_satser/if_sats2.js
+++ PRRPRR01/If_satser/if_sats2.js
@@ -1,12 +1,11 @@
 console.log("hej detta är if_sats.lektion2")
 
-/* skriv en programm som låter användaren mata in sin ålder. Programmet ska avgöra om personen får kolla på en 15-årsgräns film på bio */
+// Uppgift
+/* en enkelbiljet: 30kr
+   en månadskort: 775kr
+   Fråga hur många gånger användaren åker spårvagn och avgör om det är värt att köpa månadskort eller inte. */
 
-/* Utöka programmet till kolla om användaren får titta på en 7-årsgräns film på bio */
 
-/* Utöka programmet till kolla om användaren får titta på en 18-årsgräns film på bio */
-
- 
 let num = prompt("Hur många gånger reser du i en månad?")
 
 let pris = 30*num
@@ -15,8 +14,3 @@ if (pris >= 775) {
 } else  {
     console.log(` Det kommer att kosta ${pris} och du behöver inte en månadskort`)
 }
-
-// Uppgift
-/* en enkelbiljet: 30kr
-   en månadskort: 775kr
-   Fråga hur många gånger användaren åker spårvagn och avgör om det är värt att köpa månadskort eller inte. */
