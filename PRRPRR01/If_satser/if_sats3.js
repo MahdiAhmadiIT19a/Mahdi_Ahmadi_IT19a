1e675b12d2af2315942071c8a1df2fe88d953e31
MahdiAhmadiIT19a <70194615+MahdiAhmadiIT19a@users.noreply.github.com>
2020-09-28 14:09:15 +0200

    den nyaste!

--- PRRPRR01/If_satser/if_sats3.js
+++ PRRPRR01/If_satser/if_sats3.js
@@ -4,7 +4,7 @@ console.log("Välkommen till SAS")
 let vikt = prompt("hur mycket väger handbaggaget? (kg)")
 
 if (vikt <= 8 ) {
-    console.log("Check!")
+    console.log("Vikten är okej!")
 } else {
     console.log("Error! sänk på vikten.")
 }
@@ -13,8 +13,8 @@ let längd = prompt("Ange längden på baggaget (cm)")
 let bredd = prompt("Ange bredden på baggaget (cm)")
 let höjd = prompt("Ange höjden på baggaget (cm)")
 
-if (längd <= 55 && bredd <= 40 && höjd <= 23){
-    console.log("Checked!")
+if (vikt <=8 && längd <= 55 && bredd <= 40 && höjd <= 23){
+    console.log("Vikten och dimentionen är okej!")
 } else {
-    console.log("Error! handbaggaget är förstort och måste lämnas in")
+    console.log("Error! handbaggaget är förstort eller förtungt och måste lämnas in!")
 }
