6447e6d129a75eb62e26bd1a32fb2c16b2c56576
MahdiAhmadiIT19a <70194615+MahdiAhmadiIT19a@users.noreply.github.com>
2020-09-29 09:44:37 +0200

    den nyaste!

--- PRRPRR01/for_satser/for1.js
+++ PRRPRR01/for_satser/for1.js
@@ -1,8 +1,27 @@
+// repeat genom for_satsen.
+
 for (let i=1 ; i<=10 ; i++) {
-console.log (`${i}.hej!`)
+console.log (`${i}.harrr harr har ha!`)
 }
-let p_for1 = document.querySelector("#for1")
+let p_for1 = document.querySelector("#talserie1")
+let p_klass = document.querySelector("#klasslista")
 
 for (let i=0 ; i<=10 ;i++) {
 p_for1.innerHTML += `${i}.hej från JS <br/>`
+}
+
+let p_summa = document.querySelector("#summa")
+p_summa.innerHTML += `1+2+3+...+9+10 = ${summa}`
+
+// skapa en lista med elever
+
+
+let elev = ["kokchun", "Henrik", "Thomas", "Tatiana", "Zsofia", "David"]
+
+p_klass.innerHTML = "klassIT19a innehåller följande elever: <br/>"
+
+console.log(elev.length)
+
+for (let k = 0; k < elev.length; k++){
+    p_klass.innerHTML += `${k+1}. ${elev[k]} <br/>`
 }
--- PRRPRR01/inlämning 1/index.js
+++ PRRPRR01/inlämning 1/index.js
@@ -1,44 +1,9 @@
-console.log("Några frågor???")
+console.log("Fyll på Informationen är du snäll!")
 
-let Name =" Mahdi Ahmadi"
-let Adress = "Tubbegatan 4"
-let Age = "18"
-let Postnummer = "44477"
-let Stad = "Stenungssund"
-let Telefon = "076218"
+let name = prompt("Vad heter du?")
+let age = prompt("hur gammal är du?")
+let adress = prompt("Ange ditt Adress")
+let postnummer = prompt("Ange ditt postnummer")
+let stad = prompt("Vilken stad bor du i?")
+let Telefon = prompt("Vad är ditt telefonnummer?")
 
-console.log("Vad heter du?")
-
-
-if (Name == 'Mahdi Ahmadi'){
-    console.log("Hur gammal är du?")
-}
-
-
-if (Age == 18){
-    console.log("Vad är din adress?")
-}
-
-
-
-if (Adress == 'Tubbegatan 4'){
-    console.log("Vad är ditt postnummer?")
-}
-
-
-
-if (Postnummer == 44477){
-    console.log("Vilken stad bor du i?")
-}
-
-
-
-if (Stad == 'Stenungssund'){
-    console.log("Vad är ditt telefonnummer?")
-}
-
-
-
-if (Telefon == 076218) {
-    console.log("Välkommen Mahdi")
-}
