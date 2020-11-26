da45b6f420ab1fe95eb8321973c6bbea26e547e5
MahdiAhmadiIT19a <70194615+MahdiAhmadiIT19a@users.noreply.github.com>
2020-11-26 09:48:58 +0100

    2020.11.26

---
+++ PRRPRR01/Palindrom/palindrom.js
@@ -0,0 +1,16 @@
+let textarea = document.querySelector("#text")
+let button= document.querySelector("#button")
+let p_display1 = document.querySelector("#display1")
+let p_display2 = document.querySelector("#display2")
+
+button.addEventListener ("click",palindrome)
+
+function palindrome() {
+let str = textarea.value
+console.log(str);
+let res = str.split("");
+console.log(res);
+let ror = str.reverse().join('');
+console.log(ror);
+}
+
